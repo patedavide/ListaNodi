b318b29857d278a43d20f1c67b6a937f9bf6d5d4
paterno.22139 <paterno.davide.studente@itispaleocapa.it>
2026-01-16 12:38:28 +0100

    classe Nodo

---
+++ Nodo.java
@@ -0,0 +1,24 @@
+public class Nodo {
+
+    private String valore;
+    private Nodo next;
+
+    public Nodo(String valore){
+        this.valore = valore;
+        this.next = null;
+    }
+
+    public String getValore() {
+        return valore;
+    }
+    public void setValore(String valore) {
+        this.valore = valore;
+    }
+    public Nodo getNext() {
+        return next;
+    }
+    public void setNext(Nodo next) {
+        this.next = next;
+    }
+
+}
